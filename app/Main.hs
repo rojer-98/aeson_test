1c841a94b81781e928da7ad51c8ac623797fb039
rojer-98 <rojermagnuson98@gmail.com>
2023-06-19 09:27:11 +0300

    WP

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,25 @@
 module Main (main) where
 
 import Lib
+import System.Environment
+import System.Exit
 
-main :: IO ()
-main = someFunc
+main = do
+    args <- getArgs
+    p_args <- parse args
+    putStrLn $ tac p_args
+
+tac = unlines . reverse . lines
+
+parse ["-h"] = usage >> exit
+parse ["-v"] = version >> exit
+parse [] = getContents
+parse fs = concat `fmap` mapM readFile fs
+
+usage = putStrLn "Usage: tac [-vh] [file ..]"
+
+version = putStrLn "Haskell tac 0.1"
+
+exit = exitWith ExitSuccess
+
+die = exitWith (ExitFailure 1)
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,18 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Lib
-    ( someFunc
-    ) where
+  ( someFunc,
+  )
+where
+
+import Control.Applicative ((<$>), (<*>))
+import Control.Monad (mzero)
+import Data.Aeson
+import Data.ByteString (ByteString)
+import Data.ByteString qualified as B
+
+readSpecificJSON :: forall a. (FromJSON a) => ByteString -> Maybe a
+readSpecificJSON = decodeStrict
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
