bedbe40becc8877263b75cc15aab56ea963dd6a2
rojer-98 <rojermagnuson98@gmail.com>
2023-06-20 06:19:40 +0300

    WP

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Lib
@@ -11,8 +12,13 @@ import Data.Aeson
 import Data.ByteString (ByteString)
 import Data.ByteString qualified as B
 
-readSpecificJSON :: forall a. (FromJSON a) => ByteString -> Maybe a
+readSpecificJSON :: (FromJSON a) => ByteString -> Maybe a
 readSpecificJSON = decodeStrict
+
+readFile :: FilePath -> Maybe ByteString
+readFile fn = mbs
+  where
+    rfm = get $ B.readFile fn
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
